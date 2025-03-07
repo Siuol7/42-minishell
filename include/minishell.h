ae3a9b64929debf5236bc6543863abb314db3e4c
Siuol7 <nguyencaotan03072000@gmail.com>
2025-03-07 10:29:57 +0200

    Upload header file

---
+++ include/minishell.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: caonguye <caonguye@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/07 10:26:37 by caonguye          #+#    #+#             */
+/*   Updated: 2025/03/07 10:26:38 by caonguye         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <stdlib.h>
+# include <sys/wait.h>
+# include <unistd.h>
+# include <termios.h>
+# include <fcntl.h>
+# include <errno.h>
+
+#endif
