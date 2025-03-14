167b915eb192911622720a8ccf3b6245f604ba2e
Siuol7 <nguyencaotan03072000@gmail.com>
2025-03-14 10:58:12 +0200

    Restruct

--- include/main/error.h
+++ include/main/error.h
@@ -1,36 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   shell_token_type.h                                 :+:      :+:    :+:   */
+/*   error.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: caonguye <caonguye@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/07 10:54:31 by caonguye          #+#    #+#             */
-/*   Updated: 2025/03/14 02:27:22 by caonguye         ###   ########.fr       */
+/*   Created: 2025/03/14 10:07:13 by caonguye          #+#    #+#             */
+/*   Updated: 2025/03/14 10:09:13 by caonguye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SHELL_TOKEN_TYPE_H
+#ifndef ERROR_H
 
-# define SHELL_TOKEN_TYPE_H
-# include "minishell.h"
+# define ERROR_H
 
-typedef enum s_token_type
+typedef enum s_error
 {
-	CMD,
-	OP_PIPE,
-	OP_AND,
-	OP_OR,
-	RD_IN,
-	RD_OUT,
-	RD_APPEND,
-	RD_HEREDOC
-}	t_token_type;
-
-typedef struct s_token
-{
-	t_token_type	type;
-	char			*val;
-}	t_token;
-
+	ERR_ALLOC
+}	t_error;
 #endif
