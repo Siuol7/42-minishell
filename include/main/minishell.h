4bd2e0863e86ee931ceff1134fcf6f3df569678b
hihi.louis <pmtriet1211@gmail.com>
2025-03-15 14:28:03 +0200

    fix norm for incomming change

--- include/main/minishell.h
+++ include/main/minishell.h
@@ -6,11 +6,7 @@
 /*   By: tripham <tripham@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/07 10:26:37 by caonguye          #+#    #+#             */
-<<<<<<< HEAD:include/minishell.h
-/*   Updated: 2025/03/15 13:56:35 by tripham          ###   ########.fr       */
-=======
-/*   Updated: 2025/03/15 11:29:18 by caonguye         ###   ########.fr       */
->>>>>>> main:include/main/minishell.h
+/*   Updated: 2025/03/15 14:27:25 by tripham          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,32 +15,19 @@
 # define MINISHELL_H
 
 # include "system_lib.h"
-<<<<<<< HEAD:include/minishell.h
-# include "../library/libft/libft.h"
-# include "parsing/shell_token_type.h"
-# include "parsing/shell_token_ft.h"
-# include "execution/ast.h"
-# include "execution/execution.h"
-=======
 # include "error.h"
 # include "../../library/libft/libft.h"
 # include "../parsing/shell_token_type.h"
 # include "../parsing/shell_token_ft.h"
->>>>>>> main:include/main/minishell.h
 
 typedef struct s_shell
 {
 	int		token_len;
 	char	*full_cmd_line;
-<<<<<<< HEAD:include/minishell.h
-	t_token	*token_list;
-	t_ast	*ast;
-=======
 	char	**splitted_cmd;
 	t_cmd	*cmd;
 	t_error	shell_err;
 	t_token	*list;
->>>>>>> main:include/main/minishell.h
 }	t_shell;
 
 //OPERATION
