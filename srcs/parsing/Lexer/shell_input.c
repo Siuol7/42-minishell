313a6c23793e77e57da9491c4d5f33d090be0915
Siuol7 <nguyencaotan03072000@gmail.com>
2025-03-16 15:57:33 +0200

    Update First Lexer Stage

--- srcs/parsing/Lexer/shell_input.c
+++ srcs/parsing/Lexer/shell_input.c
@@ -6,7 +6,7 @@
 /*   By: caonguye <caonguye@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/07 10:58:30 by caonguye          #+#    #+#             */
-/*   Updated: 2025/03/16 04:41:04 by caonguye         ###   ########.fr       */
+/*   Updated: 2025/03/16 15:56:21 by caonguye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,9 @@ void	shell_input(t_shell	*mns)
 	while (1)
 	{
 		mns->full_cmd_line = readline("minishell$ ");
+		if (!ft_strcmp(mns->full_cmd_line, "exit"))
+			return ;
 		shell_token_gen(mns, mns->full_cmd_line);
-		printf("Token len: %d\n", mns->token_cnt);
-		for (int i = 0; i < mns->token_cnt; i++)
-			printf("Token %d : %s\n", i, mns->list[i].val);
-		for (int j = 0; j < mns->cmd_cnt; j++)
-		{
-			printf("Cmd %d, %s\n", j, mns->cmd[j].cmd);
-			for (int k = 0; k < mns->cmd[j].size; k++)
-				printf("Cmd group %d : %s ", j, mns->cmd[j].cmd_gr[k]);
-		}
 		free(mns->full_cmd_line);
 		ft_free_2d((void **)mns->splitted_cmd);
 		free(mns->list);
