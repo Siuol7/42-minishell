259e2d99eaf9e3984d9ebd6d8f978e84bce9540e
Siuol7 <nguyencaotan03072000@gmail.com>
2025-03-28 10:44:18 +0200

    Handle newline input

--- Srcs/Main/shell_input.c
+++ Srcs/Main/shell_input.c
@@ -6,11 +6,26 @@
 /*   By: caonguye <caonguye@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/07 10:58:30 by caonguye          #+#    #+#             */
-/*   Updated: 2025/03/27 11:36:03 by caonguye         ###   ########.fr       */
+/*   Updated: 2025/03/28 10:42:45 by caonguye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static void	shell_input_operate(t_shell *mns)
+{
+	prs_extra_check(mns);
+	add_history(mns->full_cmd_line);
+	shell_token_gen(mns, mns->full_cmd_line);
+	if (prs_cmd_check(mns))
+	{
+		printf("%s\n", mns->full_cmd_line);
+		mns->ast = ast_root_init(mns->list, mns->token_cnt);
+		if (!mns->ast)
+			shell_clean(mns);
+		exec_ast(mns, mns->ast);
+	}
+}
 
 void	shell_input(t_shell	*mns)
 {
@@ -22,17 +37,8 @@ void	shell_input(t_shell	*mns)
 			printf("exit");
 			shell_clean(mns);
 		}
-		prs_extra_check(mns);
-		add_history(mns->full_cmd_line);
-		shell_token_gen(mns, mns->full_cmd_line);
-		if (prs_cmd_check(mns))
-		{
-			printf("%s\n", mns->full_cmd_line);
-			mns->ast = ast_root_init(mns->list, mns->token_cnt);
-			if (!mns->ast)
-				shell_clean(mns);
-			exec_ast(mns, mns->ast);
-		}
+		if (mns->full_cmd_line[0])
+			shell_input_operate(mns);
 		shell_pre_input(mns);
 	}
 }
