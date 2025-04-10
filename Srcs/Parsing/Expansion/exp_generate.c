a9858ea366322a591aafac87068aba59c005137f
Siuol7 <nguyencaotan03072000@gmail.com>
2025-04-11 02:18:20 +0300

    Update temp

--- Srcs/Parsing/Expansion/exp_generate.c
+++ Srcs/Parsing/Expansion/exp_generate.c
@@ -1,19 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   exp_validation.c                                   :+:      :+:    :+:   */
+/*   exp_generate.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: caonguye <caonguye@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 00:35:59 by caonguye          #+#    #+#             */
-/*   Updated: 2025/04/10 11:12:51 by caonguye         ###   ########.fr       */
+/*   Updated: 2025/04/11 02:17:57 by caonguye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void	exp_check(t_shell *mns, t_cmd *gr)
+static void	exp_check(char *str)
 {
+	int	i;
+	int	size;
+	int	open;
+
+	i = 0;
+	size = ft_strlen(str);
+	while (i < size)
+	{
+		exp_check_open(str[i], &open)
+	}
 }
 
 void	exp_generate(t_shell *mns)
@@ -27,6 +37,9 @@ void	exp_generate(t_shell *mns)
 		j = 0;
 		while (j < mns->cmd_group[i].token_cnt)
 		{
-			
+			if (exp_check(mns->cmd_group[i].token[j]))
+				printf("yes");
+			j++;
 		}
+	}
 }
