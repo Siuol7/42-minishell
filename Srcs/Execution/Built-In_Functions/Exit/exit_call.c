395a9c0916ae222f04af9319acf31344a8a5187d
hihi.louis <pmtriet1211@gmail.com>
2025-04-14 17:17:18 +0300

    update correct bi_exit

--- Srcs/Execution/Built-In_Functions/Exit/exit_call.c
+++ Srcs/Execution/Built-In_Functions/Exit/exit_call.c
@@ -6,28 +6,44 @@
 /*   By: tripham <tripham@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/23 03:34:33 by caonguye          #+#    #+#             */
-/*   Updated: 2025/04/14 16:43:20 by tripham          ###   ########.fr       */
+/*   Updated: 2025/04/11 21:22:07 by tripham          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void	exit_standalone(t_shell *mns)
+// static void	exit_standalone(t_shell *mns)
+// {
+// 	printf("exit\n");
+// 	shell_clean(mns);
+// 	exit(0);
+// }
+
+static int	is_numeric(const char *str)
 {
-	printf("exit\n");
-	shell_clean(mns);
-	exit(0);
+	int	i;
+
+	i = 0;
+	if (str[i] == '-' || str[i] == '+')
+		i++;
+	while (str[i])
+	{
+		if (!ft_isdigit(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
 void	bi_exit(t_shell *mns, t_cmd *cmd)
 {
 	long	code;
 
+	printf("exit\n");
 	if (cmd->arg_cnt == 1)
 	{
-		code = mns->exitcode;
 		shell_clean(mns);
-		exit (code);
+		exit (mns->exitcode);
 	}
 	if (!is_numeric(cmd->cmd_arg[1]))
 	{
